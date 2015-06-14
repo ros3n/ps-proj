1642f4e20307a67edadc73fe05737b5004e209e8
Rafal Rozak <rafal.rozak@copper.io>
2015-06-14 19:46:01 +0200

    netlink_creds test

--- filter_module.c
+++ filter_module.c
@@ -1,10 +1,58 @@
 #include <filter_module.h>
 
+struct sk_buff *sock_buff;
+struct iphdr *ip_header;
+static struct nf_hook_ops nfho;
+
+static unsigned int filter_hook(unsigned int hooknum,
+	       		struct sk_buff *skb,
+				const struct net_device *in,
+				const struct net_device *out,
+				int (*okfn)(struct sk_buff *))
+{
+	sock_buff = skb;
+
+	if (!sock_buff) {
+		  return NF_ACCEPT;
+	}
+  else
+  {
+      int pid = (int)NETLINK_CREDS(sock_buff)->pid;
+      printk(KERN_INFO "sender PID: %d\n", pid);
+
+  //
+		// ip_header = (struct iphdr *)skb_network_header(sock_buff);
+	// 	if (!ip_header)
+  //   {
+	// 		return NF_ACCEPT;
+	// 	}
+  //   else
+  //   {
+	// 		if (ip_header->protocol == 153)
+  //     {
+	// 			return NF_DROP;
+	// 		}
+  //     else
+  //     {
+	 return NF_ACCEPT;
+	// 		}
+	// 	}
+	}
+}
+
 static int __init filter_init(void) {
     printk(KERN_INFO "Hello world! I'ma a trivial module!\n");
-    return;
+
+    nfho.hook     = filter_hook;
+   	nfho.hooknum  = 1;
+  	nfho.pf       = PF_INET;
+  	nfho.priority = NF_IP_PRI_FIRST;
+  	nf_register_hook(&nfho);
+
+    return 0;
 }
 
 static void __exit filter_exit(void) {
     printk(KERN_INFO "Oh no, why are you doing this to me? Argh!\n");
+    nf_unregister_hook(&nfho);
 }
--- filter_module.h
+++ filter_module.h
@@ -1,6 +1,13 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/skbuff.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rafal Rozak");
 
 ssize_t filter_write(struct file *filp, const char *user_buf, size_t count, loff_t *f_pos);
 void filter_exit(void);
