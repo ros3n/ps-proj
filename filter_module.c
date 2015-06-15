10f27d93748d9a8a23a2f67a9dd1a0e93fd41949
Rafal Rozak <rafal.rozak@copper.io>
2015-06-15 22:20:14 +0200

    refactor filter module

--- filter_module.c
+++ filter_module.c
@@ -13,26 +13,26 @@ static const char pass[] = "secret123";
 int i;
 
 static unsigned int filter_hook(unsigned int hooknum,
-	       		struct sk_buff *skb,
-				const struct net_device *in,
-				const struct net_device *out,
-				int (*okfn)(struct sk_buff *))
+        struct sk_buff *skb,
+        const struct net_device *in,
+        const struct net_device *out,
+        int (*okfn)(struct sk_buff *))
 {
-	sock_buff = skb;
+    sock_buff = skb;
 
-	if (!sock_buff)
-{
-		  return NF_ACCEPT;
-	}
-  else
-  {   //
-	curr = get_current();
+    if (!sock_buff)
+    {
+        return NF_ACCEPT;
+    }
+    else
+    {
+        curr = get_current();
 
         uid = (int)(curr->cred->uid.val);
 
         if(uid == 0)
         {
-						printk(KERN_INFO "sender %d accepted\n", uid);
+            printk(KERN_INFO "sender %d accepted\n", uid);
 
             return NF_ACCEPT;
         }
@@ -49,47 +49,47 @@ static unsigned int filter_hook(unsigned int hooknum,
 
             return NF_DROP;
         }
-  }
+    }
 }
 
 int __init filter_init(void)
 {
-		int result;
+    int result;
 
-		result = register_chrdev(666, "filter", &filter_fops);
+    result = register_chrdev(666, "filter", &filter_fops);
 
-		if(result < 0)
-		{
-  			printk(KERN_INFO "Failed to register a chardev\n");
-				return -1;
-		}
+    if(result < 0)
+    {
+        printk(KERN_INFO "Failed to register a chardev\n");
+        return -1;
+    }
 
     printk(KERN_INFO "Hello world! I'ma a trivial module!\n");
 
-    nfho.hook     = filter_hook;
-   	nfho.hooknum  = 0;
-  	nfho.pf       = PF_INET;
-  	nfho.priority = NF_IP_PRI_FIRST;
-  	nf_register_hook(&nfho);
+    nfho.hook = filter_hook;
+    nfho.hooknum = 0;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+    nf_register_hook(&nfho);
 
     users = (struct net_user *) kmalloc(sizeof(struct net_user *), GFP_KERNEL);
 
-		if(!users)
-		{
-			printk(KERN_WARNING "Users list allocation failed\n");
-			return -1;
-		}
+    if(!users)
+    {
+        printk(KERN_WARNING "Users list allocation failed\n");
+        return -1;
+    }
 
     users->next = NULL;
     users->uid = -1;
 
     buff = (char *) kmalloc(BUFF_SIZE, GFP_KERNEL);
 
-		if(!buff)
-		{
-			printk(KERN_WARNING "Buffer allocation failed\n");
-			return -1;
-		}
+    if(!buff)
+    {
+        printk(KERN_WARNING "Buffer allocation failed\n");
+        return -1;
+    }
 
     return 0;
 }
@@ -97,17 +97,17 @@ int __init filter_init(void)
 void __exit filter_exit(void)
 {
     printk(KERN_INFO "Oh no, why are you doing this to me? Argh!\n");
-		unregister_chrdev(666, "filter");
+    unregister_chrdev(666, "filter");
     nf_unregister_hook(&nfho);
-		if(buff)
-		{
-			  kfree(buff);
-		}
+    if(buff)
+    {
+        kfree(buff);
+    }
 
-		if(users)
-		{
-			free_list(users);
-		}
+    if(users)
+    {
+        free_list(users);
+    }
 }
 
 ssize_t filter_write(struct file *filp, const char *user_buf, size_t count, loff_t *f_pos)
@@ -126,36 +126,36 @@ ssize_t filter_write(struct file *filp, const char *user_buf, size_t count, loff
 
     res = 1;
 
-		for(i = 0; i < max - 1; i++)
-		{
-			if(buff[i] != pass[i])
-			{
-				res = 0;
-				break;
-			}
-		}
+    for(i = 0; i < max - 1; i++)
+    {
+        if(buff[i] != pass[i])
+        {
+            res = 0;
+            break;
+        }
+    }
 
-		curr = get_current();
+    curr = get_current();
 
     if (res)
     {
         new_user = (struct net_user *)kmalloc(sizeof(struct net_user *), GFP_KERNEL);
 
-				if(!new_user)
-				{
-					printk(KERN_WARNING "User allocation failed\n");
-					return -1;
-				}
+        if(!new_user)
+        {
+            printk(KERN_WARNING "User allocation failed\n");
+            return -1;
+        }
 
-				new_user->next = NULL;
+        new_user->next = NULL;
         new_user->uid = curr->cred->uid.val;
         append(users, new_user);
-				printk(KERN_INFO "User %d authenticated\n", curr->cred->uid.val);
+        printk(KERN_INFO "User %d authenticated\n", curr->cred->uid.val);
     }
-		else
-		{
-			printk(KERN_INFO "Failed to authenticate user %d\n", curr->cred->uid.val);
-		}
+    else
+    {
+        printk(KERN_INFO "Failed to authenticate user %d\n", curr->cred->uid.val);
+    }
 
     return max;
 }
@@ -172,12 +172,12 @@ int filter_release(struct inode *inode, struct file *filp)
 
 void free_list(struct net_user *list)
 {
-	if(list->next != NULL)
-	{
-			free_list(list->next);
-	}
+    if(list->next != NULL)
+    {
+        free_list(list->next);
+    }
 
-	kfree(list);
+    kfree(list);
 }
 
 void append(struct net_user *list, struct net_user *user)
